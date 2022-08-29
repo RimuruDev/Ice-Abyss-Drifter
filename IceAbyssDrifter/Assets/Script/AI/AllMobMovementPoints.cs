b8efc910e52628cdee03ec8207f1879564311390
Rimuru <Rimuru.Dev@gmail.com>
2022-08-29 19:08:23 +0300

    This array stores absolutely all points for the movement of mobs on the map.

---
+++ IceAbyssDrifter/Assets/Script/AI/AllMobMovementPoints.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace RimuruDev.AI
+{
+    public sealed class AllMobMovementPoints : MonoBehaviour
+    {
+        [SerializeField] private Transform[] allPoints;
+
+        public Transform[] AllPoints { get => allPoints; set => allPoints = value; }
+    }
+}
