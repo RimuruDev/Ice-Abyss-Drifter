bbb518d0b3fe76b2e70d7195c141ca89a4b65f94
Rimuru <Rimuru.Dev@gmail.com>
2022-08-29 19:12:58 +0300

    Create WorldPontArray.cs

---
+++ IceAbyssDrifter/Assets/Script/AI/WorldPontArray.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace RimuruDev.AI
+{
+    public sealed class WorldPontArray : MonoBehaviour
+    {
+        [SerializeField] private Transform[] worldMovementPoints;
+
+        public Transform[] WorldMovementPoints => worldMovementPoints;
+    }
+}
