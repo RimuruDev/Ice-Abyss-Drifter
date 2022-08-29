4fad551b13be4089ce4a5cb69d1a879f07c78556
Rimuru <Rimuru.Dev@gmail.com>
2022-08-29 19:06:00 +0300

    The helper for displaying points on the level has been implemented.
    Thanks to this class, I can follow the movement of mobs along the gizmo lines.

---
+++ IceAbyssDrifter/Assets/Script/AI/OnDrawGizmosHelper.cs
@@ -0,0 +1,44 @@
+﻿using RimuruDev.AI;
+using UnityEngine;
+
+namespace RimuruDev.Helpers
+{
+    public sealed class OnDrawGizmosHelper : MonoBehaviour
+    {
+        [SerializeField] private bool isVisualization;
+
+        private BeePointArray beePointArray = null;
+        private WorldPontArrsy worldPontArray = null;
+
+        private void Awake() => Init();
+
+        private void OnValidate() => Init();
+
+        private void OnDrawGizmos()
+        {
+            if (!isVisualization) return;
+
+            VisualizationBeeTargetsPoint(beePointArray.BeeMovementPoints, Color.red);
+            VisualizationBeeTargetsPoint(worldPontArray.WorldMovementPoints, Color.blue);
+        }
+
+        private void VisualizationBeeTargetsPoint(Transform[] points, Color color)
+        {
+            var currenpPoint = points[0].position;
+
+            for (var i = 0; i < points.Length; i++)
+            {
+                Debug.DrawLine(currenpPoint, points[i].position, color);
+            }
+        }
+
+        private void Init()
+        {
+            if (beePointArray == null)
+                beePointArray = FindObjectOfType<BeePointArray>();
+
+            if (worldPontArray == null)
+                worldPontArray = FindObjectOfType<WorldPontArrsy>();
+        }
+    }
+}
