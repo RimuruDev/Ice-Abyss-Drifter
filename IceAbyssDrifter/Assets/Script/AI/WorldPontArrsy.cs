a7c83c501820164649d412c34d33fd27c3628e0d
Rimuru <Rimuru.Dev@gmail.com>
2022-08-29 19:04:52 +0300

    Added an array with all mob movement points.
    This array stores absolutely all points for the movement of mobs on the map.

---
+++ IceAbyssDrifter/Assets/Script/AI/WorldPontArrsy.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace RimuruDev.AI
+{
+    public sealed class WorldPontArrsy : MonoBehaviour
+    {
+        [SerializeField] private Transform[] worldMovementPoints;
+
+        public Transform[] WorldMovementPoints => worldMovementPoints;
+    }
+}
