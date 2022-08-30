435a09757f2082f4b2e45f73e9bb7f1b2de76f43
Rimuru <Rimuru.Dev@gmail.com>
2022-08-30 08:36:19 +0300

    Added implicit access modifiers.

--- IceAbyssDrifter/Assets/Script/Mahagers/Directs.cs
+++ IceAbyssDrifter/Assets/Script/Mahagers/Directs.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Directs : MonoBehaviour
+public sealed class Directs : MonoBehaviour
 {
-    [SerializeField] GameObject _posters;
+    [SerializeField] private GameObject _posters;
 
-    [SerializeField] Text _dirX;
-    [SerializeField] Text _dirY;
+    [SerializeField] private Text _dirX;
+    [SerializeField] private Text _dirY;
 
-    void Update()
+    private void Update()
     {
         _dirX.text = "X: " + _posters.transform.position.x.ToString();
         _dirY.text = "Y: " + _posters.transform.position.y.ToString();
