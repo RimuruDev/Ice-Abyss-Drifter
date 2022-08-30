0e6b5852ec02c2d4976068ce61474029dfed3ecf
Rimuru <Rimuru.Dev@gmail.com>
2022-08-30 08:37:09 +0300

    Added implicit access modifiers + sealed class.

--- IceAbyssDrifter/Assets/Script/Mahagers/ManagerInvent.cs
+++ IceAbyssDrifter/Assets/Script/Mahagers/ManagerInvent.cs
@@ -1,34 +1,31 @@
 using RimuruDev.Mechanics.Character;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ManagerInvent : MonoBehaviour
+public sealed class ManagerInvent : MonoBehaviour
 {
-    [SerializeField] GameObject _inventore;
+    [SerializeField] private GameObject _inventore;
 
     private bool _isOpenInventore = false;
 
     public static bool _inventorOpen;
 
-    void Awake()
-    {
-        _isOpenInventore = false;
-    }
+    private void Awake() => _isOpenInventore = false;
 
-    void Update()
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && DeadPlayer._isDead == false && LutingPlayer._isMagazineOpen == false && GameManager._isPaused == false)
         {
             _isOpenInventore = true;
             _inventorOpen = true;
-            Time.timeScale = 0;       
+
+            Time.timeScale = 0;
         }
 
         if (Input.GetKeyUp(KeyCode.E) && DeadPlayer._isDead == false && LutingPlayer._isMagazineOpen == false && GameManager._isPaused == false)
         {
             _isOpenInventore = false;
             _inventorOpen = false;
+
             Time.timeScale = 1;
         }
 
