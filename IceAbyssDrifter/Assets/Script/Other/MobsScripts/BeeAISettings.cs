cabbae44c5eba0b5c97f3cb2947615341a22a7c1
Rimuru <Rimuru.Dev@gmail.com>
2022-08-30 08:45:04 +0300

    Added BeeAISettings

---
+++ IceAbyssDrifter/Assets/Script/Other/MobsScripts/BeeAISettings.cs
@@ -0,0 +1,12 @@
+﻿namespace RimuruDev.AI
+{
+    [System.Serializable]
+    public class BeeAISettings
+    {
+        public float motionSpeed = 6f;
+        public float startWaitTime;
+        public float minDistance;
+
+        public bool isBee = true;
+    }
+}
