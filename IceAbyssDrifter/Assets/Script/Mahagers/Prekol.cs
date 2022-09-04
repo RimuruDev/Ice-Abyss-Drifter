75d451d016449dbdc8796da902ddb514cff82ee2
Rimuru <85500556+RimuruDev@users.noreply.github.com>
2022-09-04 22:59:41 +0400

    Update Prekol.cs

--- IceAbyssDrifter/Assets/Script/Mahagers/Prekol.cs
+++ IceAbyssDrifter/Assets/Script/Mahagers/Prekol.cs
@@ -2,29 +2,29 @@ using UnityEngine;
 
 public sealed class Prekol : MonoBehaviour
 {
-    [SerializeField] private AudioClip _pigstep;
-    [SerializeField] private AudioClip _gameThemeclip;
-    [SerializeField] private AudioClip _wait;
+    [SerializeField] private AudioClip pigstep;
+    [SerializeField] private AudioClip gameThemeclip;
+    [SerializeField] private AudioClip wait;
 
-    [SerializeField] private AudioSource _gameTheme;
+    [SerializeField] private AudioSource gameTheme;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.F1))
         {
-            _gameTheme.clip = _pigstep;
-            _gameTheme.Play();
+            gameTheme.clip = pigstep;
+            gameTheme.Play();
         }
 
         if (Input.GetKeyDown(KeyCode.F2))
         {
-            _gameTheme.clip = _gameThemeclip;
-            _gameTheme.Play();
+            gameTheme.clip = gameThemeclip;
+            gameTheme.Play();
         }
         if (Input.GetKeyDown(KeyCode.F3))
         {
-            _gameTheme.clip = _wait;
-            _gameTheme.Play();
+            gameTheme.clip = wait;
+            gameTheme.Play();
         }
     }
 }
