8a15581d7359834d648a7d4ad6eb26b94b38b51e
Rimuru <Rimuru.Dev@gmail.com>
2022-09-09 21:48:56 +0300

    Update GerySheet.cs

--- IceAbyssDrifter/Assets/Script/Other/Player/GerySheet.cs
+++ IceAbyssDrifter/Assets/Script/Other/Player/GerySheet.cs
@@ -6,14 +6,14 @@ public sealed class GerySheet : MonoBehaviour
     {
         if (_coll.gameObject.CompareTag("Gary") && LutingPlayer._heKeng == false)
         {
-            GameManager._uranClear = 0f;
-            GameManager._uran = 0f;
-            GameManager._metal = 0f;
-            GameManager._pointUgly = 0f;
-            GameManager._horny = 0f;
-            GameManager._metalBullet = 0f;
-            GameManager._clearRubin = 0f;
-            GameManager._rubin = 0f;
+            dataContainer._uranClear = 0f;
+            dataContainer._uran = 0f;
+            dataContainer._metal = 0f;
+            dataContainer._pointUgly = 0f;
+            dataContainer._horny = 0f;
+            dataContainer._metalBullet = 0f;
+            dataContainer._clearRubin = 0f;
+            dataContainer._rubin = 0f;
         }
     }
 }
