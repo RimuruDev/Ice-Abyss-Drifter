0c2e01db83a06ba7f2518768fc56be150bc83197
Rimuru <Rimuru.Dev@gmail.com>
2022-09-09 22:06:56 +0300

    Update KingAI.cs

--- IceAbyssDrifter/Assets/Script/Other/MobsScripts/KingAI.cs
+++ IceAbyssDrifter/Assets/Script/Other/MobsScripts/KingAI.cs
@@ -2,22 +2,22 @@ using UnityEngine;
 
 public sealed class KingAI : MonoBehaviour
 {
-    [SerializeField] private float _speed;
+    [SerializeField] private float  speed;
 
     private void FixedUpdate()
     {
-        transform.position = Vector2.MoveTowards(transform.position, CharacterController._playerPoint.transform.position, _speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, CharacterController. playerPoint.transform.position,  speed * Time.deltaTime);
 
         FlipWithPlayer();
     }
 
     private void FlipWithPlayer()
     {
-        if (CharacterController._playerPoint.transform.position.x < transform.position.x)
+        if (CharacterController. playerPoint.transform.position.x < transform.position.x)
         {
             transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
-        else if (CharacterController._playerPoint.transform.position.x > transform.position.x)
+        else if (CharacterController. playerPoint.transform.position.x > transform.position.x)
         {
             transform.localRotation = Quaternion.Euler(0, 180, 0);
         }
