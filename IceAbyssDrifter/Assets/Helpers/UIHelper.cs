172e8693da713a7b3aef1536a040c41bf3c3f34e
Rimuru <Rimuru.Dev@gmail.com>
2022-09-10 19:33:48 +0300

    Removed the old version of the project.

--- IceAbyssDrifter/Assets/Helpers/UIHelper.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace RimuruDev.Helpers
-{
-    public sealed class UIHelper : MonoBehaviour
-    {
-        public void OpenAndClose(GameObject window) => window.SetActive(!window.activeInHierarchy);
-    }
-}
--- IceAbyssDrifter/Assets/Script/Other/GunScripts/GunTransform.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public sealed class GunTransform : MonoBehaviour
-{
-    [SerializeField] private Transform  player;
-
-    private void Update() => gameObject.transform.position =  player.transform.position;
-}
