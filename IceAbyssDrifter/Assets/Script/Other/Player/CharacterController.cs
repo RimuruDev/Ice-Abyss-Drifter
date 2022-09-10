63b043277ff3075d0435f10ac6d49a6dc776d30d
Rimuru <Rimuru.Dev@gmail.com>
2022-09-10 14:45:13 +0300

    Update CharacterController.cs

--- IceAbyssDrifter/Assets/Script/Other/Player/CharacterController.cs
+++ IceAbyssDrifter/Assets/Script/Other/Player/CharacterController.cs
@@ -9,33 +9,36 @@ public sealed class CharacterController : MonoBehaviour
 
     [SerializeField] private Joystick joystick;
 
-    [SerializeField] private Transform  player;
+    [SerializeField] private Transform player;
 
-    [SerializeField] private GameObject  effectTeleport;
+    [SerializeField] private GameObject effectTeleport;
 
-    [SerializeField] private Transform[]  pointStart;
-    private int  random;
+    [SerializeField] private Transform[] pointStart;
+    private int random;
     [Space]
     public SpriteRenderer characterSpriteRenderer;
 
-    public static float  speed = 7f;
-    public static Transform  playerPoint;
+    public static float speed = 7f;
+    public static Transform playerPoint;
 
-    private float  timeStart = 7f;
-    private float  timeShot = 7f;
+    private float timeStart = 7f;
+    private float timeShot = 7f;
 
-    private Rigidbody2D  rb;
-    private Vector2  movement;
+    private Rigidbody2D rb;
+    private Vector2 movement;
 
     private void Awake()
     {
         gameContainer = FindObjectOfType<GameDataContainer>();
+    }
 
-         timeStart = Random.Range(5, 12);
-         timeShot =  timeStart;
-         random = Random.Range(0,  pointStart.Length);
-         rb = GetComponent<Rigidbody2D>();
-        transform.position =  pointStart[ random].transform.position;
+    private void Start()
+    {
+        timeStart = Random.Range(5, 12);
+        timeShot = timeStart;
+        random = Random.Range(0, pointStart.Length);
+        rb = GetComponent<Rigidbody2D>();
+        transform.position = pointStart[random].transform.position;
 
         if (characterSpriteRenderer == null)
             characterSpriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
@@ -43,32 +46,32 @@ public sealed class CharacterController : MonoBehaviour
 
     private void FixedUpdate()
     {
-         movement = GetPlayerInput();
+        movement = GetPlayerInput();
 
         // textX.text = transform.position.x.ToString() + " :X";
         // textY.text = transform.position.y.ToString() + " :Y";
 
-        if (!( playerPoint ==  player))
-             playerPoint =  player;
+        if (!(playerPoint == player))
+            playerPoint = player;
 
-        if ( movement.x < 0)
+        if (movement.x < 0)
             FlipX(false);
 
-        if ( movement.x > 0)
+        if (movement.x > 0)
             FlipX(true);
 
         if (gameContainer.Cosmo >= 1f)
         {
-            if ( timeShot <= 0f)
+            if (timeShot <= 0f)
             {
                 transform.position = new Vector2(Random.Range(110, -110), Random.Range(70, 10));
-                 timeStart = Random.Range(5, 12);
-                Instantiate( effectTeleport, gameObject.transform.position, Quaternion.identity);
-                 timeShot =  timeStart;
+                timeStart = Random.Range(5, 12);
+                Instantiate(effectTeleport, gameObject.transform.position, Quaternion.identity);
+                timeShot = timeStart;
             }
             else
             {
-                 timeShot -= Time.deltaTime;
+                timeShot -= Time.deltaTime;
             }
         }
 
@@ -77,9 +80,9 @@ public sealed class CharacterController : MonoBehaviour
 
     public Vector2 GetPlayerInput() => new(joystick.Horizontal, joystick.Vertical);
 
-    public void CharacterMotion() =>  rb.velocity = GetPlayerInput().normalized *  speed;
+    public void CharacterMotion() => rb.velocity = GetPlayerInput().normalized * speed;
 
     public void FlipX(bool isFlipX) => characterSpriteRenderer.flipX = isFlipX;
 
-    public Transform GetCharacterTransform { get =>  player; private set =>  player = value; }
+    public Transform GetCharacterTransform { get => player; private set => player = value; }
 }
