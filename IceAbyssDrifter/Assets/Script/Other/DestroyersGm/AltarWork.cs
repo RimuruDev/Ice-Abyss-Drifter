7821d258db8a0fd881c2a88f56ce90dd17da2cd9
Rimuru <Rimuru.Dev@gmail.com>
2022-09-10 19:38:55 +0300

    Latest version of Ice Wasterland v2.0.0 is loaded

---
+++ IceAbyssDrifter/Assets/Script/Other/DestroyersGm/AltarWork.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AltarWork : MonoBehaviour
+{
+    [SerializeField] GameObject _effectAltar;
+
+    [SerializeField] GameObject _heartImage;
+    [SerializeField] GameObject _south;
+
+    [SerializeField] AudioSource _aktiv;
+
+    void OnTriggerEnter2D(Collider2D _coll)
+    {
+        if (_coll.gameObject.CompareTag("Player") && SouthPickUp._isPick == true)
+        { 
+            Instantiate(_effectAltar, gameObject.transform.position, Quaternion.identity);
+            _south.SetActive(false);
+            _aktiv.Play();
+            _heartImage.SetActive(true);
+            SouthPickUp._isLive = true;
+            SouthPickUp._isPick = false;
+            Destroy(gameObject);
+        }
+    }
+}
