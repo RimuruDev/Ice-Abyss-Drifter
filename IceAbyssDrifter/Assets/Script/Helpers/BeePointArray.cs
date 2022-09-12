3710fad0b4c49622a3027d426a45baffba6d050f
Rimuru <Rimuru.Dev@gmail.com>
2022-09-12 19:20:44 +0300

    Added array for bee movement points.

---
+++ IceAbyssDrifter/Assets/Script/Helpers/BeePointArray.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace RimuruDev
+{
+    public sealed class BeePointArray : MonoBehaviour
+    {
+        [SerializeField] private Transform[] beeMovementPoints;
+
+        public Transform[] BeeMovementPoints => beeMovementPoints;
+    }
+}
