7ad70b50a058b651ed36526aa6b1c70f311302a1
Rimuru <Rimuru.Dev@gmail.com>
2022-09-13 17:13:14 +0300

    A container has been created that stores all the effects for ores. :+1:

---
+++ IceAbyssDrifter/Assets/Script/Ores/OreDestructionEffectContainer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace RimuruDev
+{
+    public sealed class OreDestructionEffectContainer : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> oreDestructionEffects;
+
+        public List<GameObject> OreDestructionEffects { get => oreDestructionEffects; set => oreDestructionEffects = value; }
+    }
+}
