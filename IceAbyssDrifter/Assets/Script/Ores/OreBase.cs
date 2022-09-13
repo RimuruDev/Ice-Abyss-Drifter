491c12913d6992de54796eb1fe4350e93defca90
Rimuru <Rimuru.Dev@gmail.com>
2022-09-13 17:13:58 +0300

    An auxiliary class is implemented which is a tag.

---
+++ IceAbyssDrifter/Assets/Script/Ores/OreBase.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace RimuruDev
+{
+    [RequireComponent(typeof(DestructionTimer))]
+    public sealed class OreBase : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        //private void OnValidate()
+        //{
+        //    if (GetComponent<DestructionTimer>().TimerToDestruction == 0)
+        //        GetComponent<DestructionTimer>().TimerToDestruction = FindObjectOfType<DestructionTimerData>().DefaultDestructionTimerForOre;
+        //}
+#endif
+    }
+}
