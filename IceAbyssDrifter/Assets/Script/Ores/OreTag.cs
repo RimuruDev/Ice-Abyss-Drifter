6a7a4cf14b39e45d621a5aebc4de073a91476e88
Rimuru <Rimuru.Dev@gmail.com>
2022-09-13 17:11:10 +0300

    Created a container that stores the names of ores.

---
+++ IceAbyssDrifter/Assets/Script/Ores/OreTag.cs
@@ -0,0 +1,19 @@
+﻿namespace RimuruDev
+{
+    public struct OreTag
+    {
+        public const string SpaceOre = "SpaceOre";
+        public const string GoldOre = "GoldOre";
+        public const string HoneyOre = "HoneyOre";
+        public const string IronOre = "IronOre";
+        public const string MoonOre = "MoonOre";
+        public const string CoalOre = "CoalOre";
+        public const string RubyOre = "RubyOre";
+        public const string TerracottaOre = "TerracottaOre";
+        public const string UraniumOre = "UraniumOre";
+
+        public const string Wool = "Wool";
+        public const string TurdOre = "TurdOre";
+
+    }
+}
