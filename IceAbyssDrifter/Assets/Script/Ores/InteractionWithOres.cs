1f18a7284b99ad1ad5d1aa65cb0ece341cb22862
Rimuru <Rimuru.Dev@gmail.com>
2022-09-13 17:14:42 +0300

    Implemented user-ore interaction class.

---
+++ IceAbyssDrifter/Assets/Script/Ores/InteractionWithOres.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace RimuruDev
+{
+    public sealed class InteractionWithOres : MonoBehaviour
+    {
+        private OreDestructionEffectContainer effectContainer;
+
+        private void Awake() => effectContainer = FindObjectOfType<OreDestructionEffectContainer>();
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (!collision.gameObject.GetComponent<OreBase>()) return;
+
+            FindOreEffectInstantiate(collision);
+
+            Destroy(collision.gameObject);
+        }
+
+        private void FindOreEffectInstantiate(Collider2D collision)
+        {
+            string oreTag = collision.gameObject.GetComponent<TagForEffects>().EffectTag + "Effect";
+
+            foreach (GameObject effect in effectContainer.OreDestructionEffects)
+            {
+                if (effect.name == oreTag)
+                    Instantiate(effect, collision.transform.position, Quaternion.identity);
+            }
+        }
+    }
+}
