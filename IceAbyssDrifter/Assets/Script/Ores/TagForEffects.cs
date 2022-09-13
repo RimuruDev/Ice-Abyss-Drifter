ddfa813e620aa8d7633ee66d8fe0c6947c95a3b1
Rimuru <Rimuru.Dev@gmail.com>
2022-09-13 17:12:14 +0300

    Created a container that stores the name or tag of the ore. :alien:

---
+++ IceAbyssDrifter/Assets/Script/Ores/TagForEffects.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace RimuruDev
+{
+    public sealed class TagForEffects : MonoBehaviour
+    {
+        [SerializeField] private string effectTag;
+
+#if UNITY_EDITOR
+        //private void OnValidate()
+        //{
+        //    if (effectTag == string.Empty)
+        //        effectTag = gameObject.name;
+        //}
+#endif
+
+        public string EffectTag { get => effectTag; set => effectTag = value; }
+    }
+}
